cf1607d45382df6b364ac65a45b183a47ba44060
Farhana Kabir <farhana.m.kabir@gmail.com>
2014-01-28 16:42:32 -0800

    initial learning app version

---
+++ doogle/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doogle::Application.config.secret_key_base = 'fff91810640f9d37be722d3e96e2e4b3cd991ec0df41c5017051f2e854a6c342f5921e9ff85fe438ec79478e290f43c7b7b54c3e7850279266ed82e44cd66b32'
